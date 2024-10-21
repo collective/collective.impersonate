1a8ae91ffaba7d0a3306f67454f640b53419d752
Johannes Raggam <thetetet@gmail.com>
2024-10-21 11:10:49 +0200

    Back to development: 1.4.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.impersonate",
-    version="1.4.1",
+    version="1.4.2.dev0",
     description="Allow administrator to impersonate another user, for "
     "debugging purposes.",
     long_description=long_description,
